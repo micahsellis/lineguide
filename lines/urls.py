15df88250c82e6dd4403b9433e79f2d83d7fa3d3
Micah Ellis <micahsellis@gmail.com>
2020-09-20 14:30:56 -0600

    add wait views and line views

--- lines/urls.py
+++ lines/urls.py
@@ -6,7 +6,13 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('lines/create/', views.LineCreate.as_view(), name='line_create'),
     path('lines/<int:line_id>/', views.lines_detail, name='detail'),
+    path('lines/<int:pk>/update/', views.LineUpdate.as_view(), name='line_update'),
+    path('lines/<int:pk>/delete/', views.LineDelete.as_view(), name='line_delete'),
     path('accounts/signup/', views.signup, name='signup'),
     path('about/', views.about, name='about'),
     path('all/', views.all_lines, name='all_lines'),
+    path('waits/create/', views.WaitCreate.as_view(), name='wait_create'),
+    path('waits/<int:wait_id>/', views.waits_detail, name='wait_detail'),
+    path('waits/<int:pk>/update/', views.WaitUpdate.as_view(), name='wait_update'),
+    path('waits/<int:pk>/delete/', views.WaitDelete.as_view(), name='wait_delete'),
 ]
--- lines/views.py
+++ lines/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
@@ -17,7 +17,7 @@ def home(request):
 
 class LineCreate(LoginRequiredMixin, CreateView):
     model = Line
-    fields = ['name', 'address', 'city', 'state', 'postal_code', 'line_type', 'category']
+    fields = ['name', 'address', 'city', 'state', 'postal_code', 'line_type', 'category', 'description']
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -47,3 +47,28 @@ def about(request):
 def all_lines(request):
     lines = Line.objects.all()
     return render(request, 'lines/all.html', {'lines': lines})
+
+
+class WaitCreate(LoginRequiredMixin, CreateView):
+    model = Wait
+    fields = ['wait_time', 'party_size']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+def waits_detail(request, line_id):
+    wait = Line.objects.get(id=wait_id)
+    return render(request, 'waits/detail.html', {'wait': wait})
+
+
+class LineUpdate(UpdateView):
+  model = Line
+  fields = ['address', 'city', 'state',
+            'postal_code', 'line_type', 'category', 'description']
+
+
+class LineDelete(DeleteView):
+  model = Line
+  success_url = '/'
