86ec0b75adb8c6aeb2b4348a259908dd195c1b54
Micah Ellis <micahsellis@gmail.com>
2020-09-23 21:42:21 -0600

    fix typo

--- lines/views.py
+++ lines/views.py
@@ -32,18 +32,17 @@ class LineCreate(LoginRequiredMixin, CreateView):
 
 
 def add_photo(request, line_id):
-    photo_file = request.FILES.get('photo-file', None)
-    if photo_file:
-        s3 = boto3.client('s3')
-        key = uuid.uuid4().hex[:6] + \
-            photo_file.name[photo_file.name.rfind('.'):]
-        try:
-            s3.upload_fileobj(photo_file, BUCKET, key)
-            url = f"{S3_BASE_URL}{BUCKET}/{key}"
-            Photo.objects.create(url=url, line_id=line_id)
-        except:
-            print('An error occurred uploading file to S3')
-    return redirect('detail', line_id=line_id)
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+    try:
+      s3.upload_fileobj(photo_file, BUCKET, key)
+      url = f"{S3_BASE_URL}{BUCKET}/{key}"
+      Photo.objects.create(url=url, line_id=line_id)
+    except:
+      print('An error occured uploading file to S3')
+  return redirect('detail', line_id=line_id)
 
 def lines_detail(request, line_id):
     line = Line.objects.get(id=line_id)
