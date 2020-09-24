c699ee798f995422bc72e427ac6060d80fb6f435
Micah Ellis <micahsellis@gmail.com>
2020-09-24 13:51:08 -0600

    fix wait time table

--- lines/services.py
+++ lines/services.py
@@ -2,12 +2,12 @@ import os
 import requests
 import json
 
-def get_yelp():
-    url = "https://api.yelp.com/v3/businesses/search?term=chinese&location=Denver"
+def get_yelp(query, locale):
+    url = f"https://api.yelp.com/v3/businesses/search?term={query}&location={locale}"
     req = requests.get(
         url, headers={'Authorization': 'Bearer %s' % os.environ['API_KEY']})
     response = req.json()
     yelps_list = []
     for bus in response['businesses']:
-        yelps_list.append(bus['id'])
+        yelps_list.append(bus)
     return yelps_list
